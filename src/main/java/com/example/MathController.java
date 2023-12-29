a0f2beacead7ca5d2b0e4108c499bed404ad4dc1
Alex Rosa <alexr@gec.inatel.br>
2023-12-29 16:28:48 -0300

    Class 55To56 - Definindo logica e os metodos de validação

--- src/main/java/com/example/MathController.java
+++ src/main/java/com/example/MathController.java
@@ -15,8 +15,25 @@ public class MathController {
 
   @RequestMapping(value = "/sum/{number1}/{number2}", method = RequestMethod.GET)
   public Double sum(@PathVariable(value = "number1") String number1,
-                    @PathVariable(value = "number2") String number2) {
-    return 10.0;
+                    @PathVariable(value = "number2") String number2) throws Exception {
+
+    if (!isNumeric(number1) || !isNumeric(number2)) {
+      throw new Exception();
+    }
+    return convertToDouble(number1) + convertToDouble(number2);
+  }
+
+  private Double convertToDouble(String strNumber) {
+    if (strNumber == null) return 0D;
+    String number = strNumber.replaceAll(",", ".");
+    if (isNumeric(number)) return Double.parseDouble(number);
+    return 0D;
+  }
+
+  private boolean isNumeric(String strNumber) {
+    if (strNumber == null) return false;
+    String number = strNumber.replaceAll(",", ".");
+    return number.matches("[-+]?[0-9]*\\.?[0-9]+");
   }
 
 }
