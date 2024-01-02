c85a063cbbb4ba3decd09773bc4b6661efb94079
Alex Rosa <alexr@gec.inatel.br>
2024-01-02 20:10:07 -0300

    Class 73 - Refactoring PersonController

--- src/main/java/com/example/controllers/PersonController.java
+++ src/main/java/com/example/controllers/PersonController.java
@@ -4,6 +4,7 @@ import com.example.model.Person;
 import com.example.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,33 +16,31 @@ public class PersonController {
   @Autowired
   private PersonServices personService;
 
-  @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public Person findById(@PathVariable(value = "id") Long id) {
     return personService.findById(id);
   }
 
-  @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public List<Person> findAll() {
     return personService.findAll();
   }
 
-  @RequestMapping(value = "", method = RequestMethod.POST,
-      produces = MediaType.APPLICATION_JSON_VALUE,
-      consumes = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   public Person create(@RequestBody Person person) {
     return personService.create(person);
   }
 
-  @RequestMapping(value = "", method = RequestMethod.PUT,
-      produces = MediaType.APPLICATION_JSON_VALUE,
-      consumes = MediaType.APPLICATION_JSON_VALUE)
+  @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   public Person update(@RequestBody Person person) {
     return personService.update(person);
   }
 
-  @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-  public void delete(@PathVariable(value = "id") Long id) {
+  @DeleteMapping(value = "/{id}")
+  //@ResponseStatus(HttpStatus.NO_CONTENT)//TODO: this is a simple way to change the response status code
+  public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
     personService.delete(id);
+    return ResponseEntity.noContent().build();
   }
 
 }
