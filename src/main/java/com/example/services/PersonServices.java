668404d4cc09ae298e588432381ca661a3101aa5
Alex Rosa <alexr@gec.inatel.br>
2024-01-02 16:22:22 -0300

    Class 65 - Retornando Listas de Objetos em Json, metodo findAll()

--- src/main/java/com/example/services/PersonServices.java
+++ src/main/java/com/example/services/PersonServices.java
@@ -3,6 +3,8 @@ package com.example.services;
 import com.example.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -10,7 +12,32 @@ import java.util.logging.Logger;
 public class PersonServices {
 
   private final AtomicLong counter = new AtomicLong();
-  private Logger logger = Logger.getLogger(PersonServices.class.getName());
+  private final Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+  public List<Person> findAll() {
+    logger.info("Finding All person!");
+
+    List<Person> persons = new ArrayList<>();
+
+    for (int i=0; i< 8; i++) {
+      Person person = mockPerson(i);
+      persons.add(person);
+    }
+
+    return persons;
+  }
+
+  private Person mockPerson(int i) {
+    Person person = new Person();
+
+    person.setId(counter.incrementAndGet());
+    person.setFirstName("Alex" + i);
+    person.setLastName("Rosa" + i);
+    person.setAddress("Cambui-MG" + i);
+    person.setGender("Male" + i);
+
+    return person;
+  }
 
   public Person findById(String id) {
     logger.info("Finding one person!");
