ce862781fed894406003cf93cfc0c472c2c3c194
Alex Rosa <alexr@gec.inatel.br>
2024-01-03 10:17:43 -0300

    Class 74to75 - Update mysql version and removed warnings

--- src/main/java/com/example/repositories/PersonRepository.java
+++ src/main/java/com/example/repositories/PersonRepository.java
@@ -2,8 +2,6 @@ package com.example.repositories;
 
 import com.example.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 }
