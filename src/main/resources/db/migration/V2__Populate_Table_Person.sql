235b1ad550b98e0254d4d8034b44bbeb8333072a
Alex Rosa <alexr@gec.inatel.br>
2024-01-03 15:39:35 -0300

    Class 91 - Migrations com Flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+    (1,'Cambui','Alex','Male','Rosa'),
+    (2,'Cambui-Mg','Laura Ellen do Halley','Female','Santos'),
+    (8,'Cambui-Mg','Dinei da bel','Female','Souza'),
+    (9,'Cambui-Mg','Dinei do lairto','Female','Souza'),
+    (10,'Cambui-Mg','joao do lairto','Female','Souza');
