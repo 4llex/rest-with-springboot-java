aaa697f1fe4f11424b7126318cd9f143a195963c
Alex Rosa <alexr@gec.inatel.br>
2024-01-04 11:33:39 -0300

    Class 101 - Implemnetando ContentNegotiation - Serializando em YML

--- src/main/java/com/example/controllers/PersonController.java
+++ src/main/java/com/example/controllers/PersonController.java
@@ -3,11 +3,12 @@ package com.example.controllers;
 import com.example.data.vo.v1.PersonVO;
 import com.example.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static com.example.util.MediaType.*;
 
 @RestController
 @RequestMapping("/api/persons/v1")
@@ -16,24 +17,24 @@ public class PersonController {
   @Autowired
   private PersonServices personService;
 
-  @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+  @GetMapping(produces = {APPLICATION_JSON, APPLICATION_XML, APPLICATION_YML})
   public List<PersonVO> findAll() {
     return personService.findAll();
   }
 
-  @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+  @GetMapping(value = "/{id}", produces = {APPLICATION_JSON, APPLICATION_XML, APPLICATION_YML})
   public PersonVO findById(@PathVariable(value = "id") Long id) {
     return personService.findById(id);
   }
 
-  @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-               produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+  @PostMapping(consumes = {APPLICATION_JSON, APPLICATION_XML, APPLICATION_YML},
+               produces = {APPLICATION_JSON, APPLICATION_XML, APPLICATION_YML})
   public PersonVO create(@RequestBody PersonVO person) {
     return personService.create(person);
   }
 
-  @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-              produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+  @PutMapping(consumes = {APPLICATION_JSON, APPLICATION_XML, APPLICATION_YML},
+              produces = {APPLICATION_JSON, APPLICATION_XML, APPLICATION_YML})
   public PersonVO update(@RequestBody PersonVO person) {
     return personService.update(person);
   }
