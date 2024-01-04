0a225f68115dccb26f1fad3d1a983e06dcdf0a9d
Alex Rosa <alexr@gec.inatel.br>
2024-01-04 16:11:55 -0300

    Class 108 - Testando geração dos links HATEOAS no service com Mockito e JUnit, findById

---
+++ src/test/java/com/example/unittests/mockito/services/PersonServicesTest.java
@@ -0,0 +1,71 @@
+package com.example.unittests.mockito.services;
+
+import com.example.model.Person;
+import com.example.repositories.PersonRepository;
+import com.example.services.PersonServices;
+import com.example.unittests.mapper.mocks.MockPerson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+class PersonServicesTest {
+
+    MockPerson input;
+
+    @InjectMocks
+    private PersonServices service;
+    @Mock
+    PersonRepository repository;
+
+    @BeforeEach
+    void setUpMocks() {
+        input = new MockPerson();
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void findById() {
+        Person person = input.mockEntity(1);
+
+        when(repository.findById(1L)).thenReturn(Optional.of(person));
+
+        var result = service.findById(1L);
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+        System.out.println(result.toString());
+        assertTrue(result.toString().contains("links: [</api/persons/v1/1>;rel=\"self\"]"));
+        assertEquals("Address Test1", result.getAddress());
+        assertEquals("First Name Test1", result.getFirstName());
+        assertEquals("Last Name Test1", result.getLastName());
+        assertEquals("Female", result.getGender());
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
