08cc24b8a46b1f0bae0d8953ed12c11d9023013d
Alex Rosa <alexr@gec.inatel.br>
2024-01-04 10:49:00 -0300

    Class 100 - Implemnetando ContentNegotiation via HEADER Parameter

--- src/main/java/com/example/config/WebConfig.java
+++ src/main/java/com/example/config/WebConfig.java
@@ -12,8 +12,18 @@ public class WebConfig implements WebMvcConfigurer {
   public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
     // Via Query Param. http://localhost/api/person/v1?mediaType=xml
+    /*
     configurer.favorParameter(true)
         .parameterName("mediaType").ignoreAcceptHeader(true)
+        .useRegisteredExtensionsOnly(false)
+        .defaultContentType(MediaType.APPLICATION_JSON)
+            .mediaType("json", MediaType.APPLICATION_JSON)
+            .mediaType("xml", MediaType.APPLICATION_XML);
+     */
+
+    // Via HEADER Param. http://localhost/api/person/v1
+    configurer.favorParameter(false)
+        .ignoreAcceptHeader(false)
         .useRegisteredExtensionsOnly(false)
         .defaultContentType(MediaType.APPLICATION_JSON)
             .mediaType("json", MediaType.APPLICATION_JSON)
