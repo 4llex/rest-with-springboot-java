7c0ec2688e7cc821f4820650bdb7f08020902223
Alex Rosa <alexr@gec.inatel.br>
2024-01-05 10:22:49 -0300

    Class 118 - Configuração basica para usar Swagger(OpenAPI)

---
+++ src/main/java/com/example/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.example.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("RESTful API with Java 17 and Spring Boot 3")
+                .version("v1")
+                .description("Some information about your API")
+                .termsOfService("https://github.com/4llex")
+                .license(
+                    new License()
+                        .name("Apache 2.0")
+                        .url("https://github.com/4llex")
+                )
+            );
+    }
+
+}
