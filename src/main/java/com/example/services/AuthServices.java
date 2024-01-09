1e71a25e272d4cec26f3fb0ff449a45a816fcac1
Alex Rosa <alexr@gec.inatel.br>
2024-01-09 20:30:33 -0300

    Class 147 - Implementando o Serviço responsavel pela Autenticação

---
+++ src/main/java/com/example/services/AuthServices.java
@@ -0,0 +1,49 @@
+package com.example.services;
+
+import com.example.data.vo.v1.security.AccountCredentialsVO;
+import com.example.data.vo.v1.security.TokenVO;
+import com.example.repositories.UserRepository;
+import com.example.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class AuthServices {
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @SuppressWarnings("rawtypes")
+    public ResponseEntity signin(AccountCredentialsVO data) {
+        try {
+            var username = data.getUsername();
+            var password = data.getPassword();
+
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+            var user = userRepository.findByUsername(username);
+
+            var tokenResponse = new TokenVO();
+            if (user != null) {
+                tokenResponse = tokenProvider.createAccessToken(username, user.getRoles());
+            } else {
+                throw new UsernameNotFoundException("Username " + username + " not found!");
+            }
+            return ResponseEntity.ok(tokenResponse);
+        } catch (Exception e) {
+            throw new BadCredentialsException("Invalid username/password supplied");
+        }
+    }
+}
