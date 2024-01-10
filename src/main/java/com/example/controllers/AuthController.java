70117655b5fe18fa9d27a0a442762898d6872e41
Alex Rosa <alexr@gec.inatel.br>
2024-01-10 10:52:09 -0300

    Class 151.1715 - Implementando o RefreshToken

--- src/main/java/com/example/controllers/AuthController.java
+++ src/main/java/com/example/controllers/AuthController.java
@@ -7,10 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Tag(name = "Authentication Endpoint")
@@ -24,7 +21,7 @@ public class AuthController {
     @Operation(summary = "Authenticates a user and return a token")
     @PostMapping(value = "/signin")
     public ResponseEntity signin(@RequestBody AccountCredentialsVO data) {
-        if (checkIfParamIsNotNull(data)) {
+        if (checkIfParamsIsNotNull(data)) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
         }
 
@@ -36,7 +33,27 @@ public class AuthController {
         return token;
     }
 
-    private static boolean checkIfParamIsNotNull(AccountCredentialsVO data) {
+    @SuppressWarnings("rawtypes")
+    @Operation(summary = "Refresh token for Authenticated user and returns a token")
+    @PutMapping(value = "/refresh/{username}")
+    public ResponseEntity refreshToken(@PathVariable(name = "username") String username, @RequestHeader("Authorization") String refreshToken) {
+        if (checkIfParamsIsNotNull(username, refreshToken)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+        }
+
+        var token = authServices.refreshToken(username, refreshToken);
+        if (token == null) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+        }
+
+        return token;
+    }
+
+    private static boolean checkIfParamsIsNotNull(String username, String refreshToken) {
+        return refreshToken == null || refreshToken.isBlank() || username == null || username.isBlank();
+    }
+
+    private static boolean checkIfParamsIsNotNull(AccountCredentialsVO data) {
         return data == null || data.getUsername() == null || data.getUsername().isBlank() ||
             data.getPassword() == null || data.getPassword().isBlank();
     }
