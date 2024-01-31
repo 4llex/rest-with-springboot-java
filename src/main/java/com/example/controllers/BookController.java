3af66e70b55178e9befa1db3a312bb609f3211f0
Alex Rosa <alexr@gec.inatel.br>
2024-01-31 11:23:37 -0300

    Class 188 (2006) [DESAFIO] - Impl. Ordenação com Direction para Books

--- src/main/java/com/example/controllers/BookController.java
+++ src/main/java/com/example/controllers/BookController.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -43,10 +45,12 @@ public class BookController {
   )
   public ResponseEntity<Page<BookVO>> findAll(
       @RequestParam(value = "page", defaultValue = "0") Integer page,
-      @RequestParam(value = "size", defaultValue = "12") Integer size
+      @RequestParam(value = "size", defaultValue = "12") Integer size,
+      @RequestParam(value = "direction", defaultValue = "asc") String direction
   ) {
 
-      Pageable pageable = PageRequest.of(page, size);
+      var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+      Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "author"));
     return ResponseEntity.ok(bookService.findAll(pageable));
   }
 
