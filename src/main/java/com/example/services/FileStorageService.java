0f15166f99d9ed0f7244149321bb49ff91d5c5c4
Alex Rosa <alexr@gec.inatel.br>
2024-02-15 14:49:22 -0300

    Class 207 (2104) - Criando o Serviço Responsavel por Armazenar o Arquivo em disco

---
+++ src/main/java/com/example/services/FileStorageService.java
@@ -0,0 +1,47 @@
+package com.example.services;
+
+import com.example.config.FileStorageConfig;
+import com.example.exceptions.FileStorageException;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    public FileStorageService(FileStorageConfig fileStorageConfig) {
+        Path path = Paths.get(fileStorageConfig.getUploadDir())
+            .toAbsolutePath().normalize();
+
+        this.fileStorageLocation = path;
+
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception e) {
+            throw new FileStorageException("Could not create the directory where the uploaded files will be stored!", e);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        try {
+            if (fileName.contains("..")) {
+                throw new FileStorageException("Sorry, filename contains invalid path sequence: " + fileName);
+            }
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+
+            return fileName;
+        } catch (Exception e) {
+            throw new FileStorageException("Could not store file " + fileName + ". Please try again!", e);
+        }
+    }
+}
