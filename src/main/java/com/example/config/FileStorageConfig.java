45ef438a291036b45ca87f27ba77150a55e37fe1
Alex Rosa <alexr@gec.inatel.br>
2024-02-15 14:18:15 -0300

    Class 205 (2102) - Definindo as Configurações de Upload e Download

---
+++ src/main/java/com/example/config/FileStorageConfig.java
@@ -0,0 +1,20 @@
+package com.example.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties(prefix = "file")
+@Configuration
+public class FileStorageConfig {
+
+    private String uploadDir;
+
+    public String getUploadDir() {
+        return uploadDir;
+    }
+
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
+
+}
